57e07d02eca700c40e0d5417212af174841bdfb7
Ensar Gök <116498667+ensargx@users.noreply.github.com>
2024-11-25 12:44:12 +0300

    warnings and errors

--- neuralc/ai/model.c
+++ neuralc/ai/model.c
@@ -12,25 +12,25 @@ matrix ai_model_predict(ai_model model, matrix data)
         // Y = Act(X * W + B)
 
         log_debug("%s: layer: %d, rows: %d, cols: %d",
-                __FUNCTION__, i, vars.rows, vars.cols);
+                __func__, i, vars.rows, vars.cols);
 
         // multiply matrix
         matrix_dot(&dot_out, model.weights[i], vars);
-        log_debug("%s: after matrix_dot: ", __FUNCTION__);
+        log_debug("%s: after matrix_dot: ", __func__);
         for (int x = 0; x < dot_out.rows; ++x)
             for (int y = 0; y < dot_out.cols; ++y)
                 log_debug("matrix[%d][%d] = %lf", x, y, matrix_get(dot_out, x, y));
 
         // add bias each term
         matrix_add_row(dot_out, model.biases[i]);
-        log_debug("%s: after matrix_add_row: ", __FUNCTION__);
+        log_debug("%s: after matrix_add_row: ", __func__);
         for (int x = 0; x < dot_out.rows; ++x)
             for (int y = 0; y < dot_out.cols; ++y)
                 log_debug("matrix[%d][%d] = %lf", x, y, matrix_get(dot_out, x, y));
 
         // Activation
         ai_activation_apply_matrix_column(dot_out, model.activation);
-        log_debug("%s: after ai_activation_apply_matrix_column: ", __FUNCTION__);
+        log_debug("%s: after ai_activation_apply_matrix_column: ", __func__);
         for (int x = 0; x < dot_out.rows; ++x)
             for (int y = 0; y < dot_out.cols; ++y)
                 log_debug("matrix[%d][%d] = %lf", x, y, matrix_get(dot_out, x, y));
@@ -39,7 +39,7 @@ matrix ai_model_predict(ai_model model, matrix data)
         matrix_swap(&dot_out, &vars);
     }
 
-    log_debug("%s: Model Predicted!", __FUNCTION__);
+    log_debug("%s: Model Predicted!", __func__);
 
     return vars;
 }
--- neuralc/matrix/matrix.c
+++ neuralc/matrix/matrix.c
@@ -8,7 +8,7 @@ void matrix_init(matrix *pMatrix, int rows, int cols)
 {
     if (rows < 0 || cols < 0)
     {
-        log_error("%s: Wrong parameters: rows || cols", __FUNCTION__);
+        log_error("%s: Wrong parameters: rows || cols", __func__);
         return;
     }
 
@@ -45,7 +45,7 @@ double matrix_get(matrix matrix, int x, int y)
     if ( x >= matrix.rows || y >= matrix.cols )
     {
         log_error("%s: parameters out of bound. x: %d, rows: %d, y: %d, cols: %d.",
-                __FUNCTION__, x, matrix.rows, y, matrix.cols);
+                __func__, x, matrix.rows, y, matrix.cols);
         return 0;
     }
 
@@ -57,7 +57,7 @@ void matrix_set(matrix matrix, int x, int y, double value)
     if ( x >= matrix.rows || y >= matrix.cols )
     {
         log_error("%s: parameters out of bound. x: %d, rows: %d, y: %d, cols: %d.",
-                __FUNCTION__, x, matrix.rows, y, matrix.cols);
+                __func__, x, matrix.rows, y, matrix.cols);
         return;
     }
 
@@ -71,7 +71,7 @@ int matrix_dot(matrix* pOut, matrix x, matrix y)
     if ( x.cols != y.rows )
     {
         log_error("%s: Matrixes cannot be dot producted! x.cols: %d, y.rows: %d.",
-                __FUNCTION__, x.cols, y.rows);
+                __func__, x.cols, y.rows);
         return 0;
     }
 
@@ -140,7 +140,7 @@ void matrix_add_row(matrix mat1, matrix mat2)
     if ( mat2.cols != 1 || mat2.rows != mat1.rows )
     {
         log_error("%s: Matrixes not aligned properly! mat2.rows: %d, mat2.cols: %d, mat1.rows: %d, mat1.cols: %d",
-                __FUNCTION__, mat2.rows, mat2.cols, mat1.rows, mat1.cols);
+                __func__, mat2.rows, mat2.cols, mat1.rows, mat1.cols);
         return;
     }
 
